2f4c66f76329df872ff2bb191d34603ecfb98209
Clément Faure <clement.faure@etudiant.univ-reims.fr>
2021-02-22 14:23:44 +0100

    Edition des articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -65,4 +65,57 @@ class ArticleController extends AbstractController
                 'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/article/edit/{id}", name="article_edit")
+     */
+    public function edit (Request $request, int $id, SluggerInterface $slugger) : Response {
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($id);
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $oldImage = $article->getImage();
+            unlink($this->getParameter('images_dossier').'/'.$oldImage);
+            $article = $form->getData();
+
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_dossier'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                }
+
+                $article->setImage($newFilename);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            /*
+             * return $this->redirectToRoute('article_show', [
+                'id' => $id,
+            ]);
+             */
+        }
+        return $this->render('article/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
 }
