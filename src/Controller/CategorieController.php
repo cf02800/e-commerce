02f3841da4c64739d1f8957718c07f042d0dc49c
Clément Faure <clement.faure@etudiant.univ-reims.fr>
2021-03-01 14:54:42 +0100

    CRUD catégories

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,19 +2,85 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/categorie", name="categorie")
+     * @Route("/categorie/new", name="categorie_create")
      */
-    public function index(): Response
+    public function create(Request $request): Response
     {
-        return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($categorie);
+            $entityManager->flush();
+        }
+
+        return $this->render(
+            'categorie/create.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    /**
+     * @Route("categorie/edit/{id}", name="categorie_edit")
+     */
+    public function edit(Request $request, int $id){
+        $categorie = $this->getDoctrine()->getRepository(Categorie::class)->find($id);
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($categorie);
+            $entityManager->flush();
+        }
+
+        return $this->render('categorie/edit.html.twig',[
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("categorie/delete/{id}", name="categorie_delete")
+     * @return RedirectResponse
+     */
+
+    public function delete(Categorie $categorie): RedirectResponse{
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($categorie);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('categorie_admin');
+    }
+
+
+    /**
+     * @Route("admin/categories", name="categories_admin")
+     */
+
+    public function getAllTypes(){
+        $categories = $this->getDoctrine()->getRepository(Categorie::class)->findAll();
+
+        return $this->render('admin/categories/list.html.twig', [
+            'categories' => $categories
         ]);
     }
+
+
 }
