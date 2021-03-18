3250dadcccfaf4ef42ce54f68476bfa1814f3438
Clément Faure <clement.faure@etudiant.univ-reims.fr>
2021-03-18 10:23:26 +0100

    Service du panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,15 +3,49 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Repository\ArticleRepository;
+use App\Service\Panier\PanierService;
 
 class PanierController extends AbstractController
 {
     /**
      * @Route("/panier", name="panier")
      */
-    public function index(): Response
+    public function index(PanierService $service)
     {
+        $panier = $service->getFullPanier();
+
+        $total = $service->getTotal();
+        
+        return $this->render('panier/index.html.twig', [
+            'items' => $panier,
+            'total' => $total,
+        ]);
     }
+
+    /**
+     * @Route("panier/add/{id}", name="panier_add")
+     */
+
+    public function add(int $id, PanierService $service) {
+
+        $service->add($id);
+        
+        return $this->redirectToRoute('panier');
+    }
+
+    /**
+     * @Route("panier/delete/{id}", name="panier_delete")
+     */
+
+     public function delete(int $id, PanierService $service) {
+         
+        $service -> remove($id);
+
+        return $this->redirectToRoute('panier');
+     }
 }
